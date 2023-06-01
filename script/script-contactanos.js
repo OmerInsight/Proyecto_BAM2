cc9203a9e9d23ec1cf6beeecd0426d6a460ad0e7
unknown <omerfakex@gmail.com>
2023-06-01 00:21:21 -0300

    todos los formularios listos y pavicon listo

---
+++ script/script-contactanos.js
@@ -0,0 +1,28 @@
+// CONFIGURACIONES DE PAGINA DE CONTACTANOS
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener referencias a los elementos del formulario
+    const nombreInput = document.getElementById("nombre-contactanos");
+    const apellidoInput = document.getElementById("apellido-contactanos");
+    const emailInput = document.getElementById("email-contactanos");
+    const enviarBtn = document.querySelector(".boton-servicios");
+    const formContactanos = document.querySelector(".form-tooltip");
+
+    // Función para mostrar el mensaje y ocultar el formulario
+    function mostrarMensaje() {
+        const mensaje = document.createElement("p");
+        mensaje.textContent = "Muchas gracias por contactarnos. Durante las próximas 24 horas estaremos respondiendo tu consulta.";
+        formContactanos.parentNode.insertBefore(mensaje, formContactanos);
+        formContactanos.style.display = "none";
+    }
+
+    // Evento click en el botón "Enviar"
+    enviarBtn.addEventListener("click", function (event) {
+        event.preventDefault(); // Evitar el envío del formulario
+
+        // Validar que se hayan completado todos los campos
+        if (nombreInput.value && apellidoInput.value && emailInput.value) {
+            mostrarMensaje();
+        }
+    });
+});
--- script/script.js
+++ script/script.js
@@ -1,23 +1,64 @@
 // CONFIGURACION DE BOTON RESPONSIVE:
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function () {
-        document.querySelector(".nav-links").
-            classList.toggle("nav-links-responsive");
-    });
-// CONFIGURACION DE BOTON CULTAR O MOSTRAR LECTURA
-let ocultar_texto_btn = document.getElementById("ocultar-texto-btn");
-let ocultar_texto = document.getElementById("ocultar-texto");
-ocultar_texto_btn.addEventListener("click", toggleText);
-function toggleText() {
-    ocultar_texto.classList.toggle("show");
 
-    if (ocultar_texto.classList.contains("show")) {
-        ocultar_texto_btn.innerHTML = "Ocultar";
-    }
-    else {
-        ocultar_texto_btn.innerHTML = "Seguir leyendo";
+
+if (window.location.pathname.endsWith("index.html")) {
+    // CONFIGURACION DE BOTON RESPONSIVE:
+    document.querySelector("button.button-menu-toggle")
+        .addEventListener("click", function () {
+            document.querySelector(".nav-links").
+                classList.toggle("nav-links-responsive");
+        });
+
+    // CONFIGURACION DE BOTON OCULTAR O MOSTRAR LECTURA
+    let ocultar_texto_btn = document.getElementById("ocultar-texto-btn");
+    let ocultar_texto = document.getElementById("ocultar-texto");
+    ocultar_texto_btn.addEventListener("click", toggleText);
+
+    function toggleText() {
+        ocultar_texto.classList.toggle("show");
+
+        if (ocultar_texto.classList.contains("show")) {
+            ocultar_texto_btn.innerHTML = "Ocultar";
+        } else {
+            ocultar_texto_btn.innerHTML = "Seguir leyendo";
+        }
     }
 }
 
+
 // CONFIGURACIONES DE PAGINA DE SERVICIOS 
+
+$(document).ready(function () {
+    if (window.location.pathname.includes("servicios.html")) {
+        $(".boton-servicios").click(function () {
+            var ingresos = parseInt($("#ingresos-servicio").val()) || 0;
+            var gastosFijos = parseInt($("#gastos-fijos-servicio").val()) || 0;
+            var gastosVariables = parseInt($("#gastos-variables-servicio").val()) || 0;
+            var nombre = $("#nombre-servicio").val();
+
+            if (ingresos > 200000 && ingresos > gastosFijos + gastosVariables) {
+                // Caso 1: Ingresos competentes
+                $("#texto-mensaje").text("Felicidades " + nombre + ", usted goza de unos ingresos competentes y además los administra muy bien. Es momento de comenzar a ahorrar e invertir.");
+                $("#imagen-mensaje").attr("src", "../img/servicios-caso-1.jpg");
+            } else if (ingresos < 200000 && ingresos > gastosFijos + gastosVariables) {
+                // Caso 2: Administración adecuada
+                $("#texto-mensaje").text("Continúa así " + nombre + ", estás administrando muy bien tus recursos. Es momento de comenzar a ahorrar.");
+                $("#imagen-mensaje").attr("src", "../img/servicios-caso-2.jpg");
+            } else if (ingresos > 200000 && ingresos < gastosFijos + gastosVariables) {
+                // Caso 3: Revisar salidas de dinero
+                $("#texto-mensaje").text(nombre + ", es momento de sentarse y revisar las salidas de dinero, encontrar y cambiar ciertos hábitos.");
+                $("#imagen-mensaje").attr("src", "../img/servicios-caso-3.jpg");
+            } else {
+                // Caso 4: Finanzas en condición delicada
+                $("#texto-mensaje").text(nombre + ", tus finanzas están en una condición muy delicada y precisan de un alto compromiso de tu parte para estabilizarlas.");
+                $("#imagen-mensaje").attr("src", "../img/servicios-caso-4.jpg");
+            }
+
+            $(".formulario-servicio").hide();
+            $("#mensaje").show();
+        });
+    }
+});
+
+
 
